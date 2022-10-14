add09511ec31c82e7b9b4e9c1b91feec116c401b
cocacola-lover <pegas620570@gmail.com>
2022-10-15 01:04:13 +0300

    created a few string classes

---
+++ server/some-strings.js
@@ -0,0 +1,10 @@
+export class JSONTypes {
+    static get greet() { return 'greet'; }
+    static get processedFrame() { return 'processed frame'; }
+    static get processRequest() { return 'process request'; }
+    static get webserviceRequest() { return 'webservice request'; }
+}
+export class ConnectionTypes {
+    static get neural() { return 'neural'; }
+    static get visitor() { return 'visitor'; }
+}
