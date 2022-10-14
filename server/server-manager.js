4ec54f336025f1daee493861992d4ba07570081c
cocacola-lover <pegas620570@gmail.com>
2022-10-14 21:30:50 +0300

    starting to work on server

---
+++ server/server-manager.js
@@ -0,0 +1,9 @@
+var WebSocketModule = require('ws');
+var fs = require('fs');
+var images = [];
+var convertImagesToBase64 = function (dir) {
+    images.push();
+};
+var server = new WebSocketModule.Server({
+    port: 8080
+});
---
+++ server/server-manager.ts
@@ -0,0 +1,16 @@
+const WebSocketModule = require('ws');
+const fs = require('fs');
+
+const images:string[] = [];
+
+const convertImagesToBase64 = (dir:string) => {
+    images.push()
+}
+
+const server = new WebSocketModule.Server({
+    port: 8080
+});
+
+
+
+
