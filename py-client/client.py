47f4c780271df314426f74c8ac9f954509b80812
qw1zzard <qw1gor.p@gmail.com>
2022-10-15 16:45:44 +0300

    Add detector, correct typos

--- py-client/client.py
+++ py-client/client.py
@@ -2,27 +2,25 @@ import asyncio
 import json
 from websockets import connect
 
-from main import get_bboxes
+from detector import get_bboxes
 
 
 greet = {
-    "type" : "greet",
-    "who" : "neural" 
+    'type': 'greet',
+    'who': 'neural'
 }
-
 
 
 def neuralNetwork(json):
     response = get_bboxes(json['frame'])
-    json["type"] = "processed frame";
-    json['rectangles'] = response;
+    json['type'] = 'processed frame'
+    json['rectangles'] = response
     return json
-    
 
 
-def neuralNetworkSimulate(json) :
-    json["checked"] = True
-    json["type"] = "processed frame";
+def neuralNetworkSimulate(json):
+    json['checked'] = True
+    json['type'] = 'processed frame'
     return json
 
 
@@ -30,16 +28,15 @@ async def handle(uri):
     async with connect(uri) as websocket:
         await websocket.send(json.dumps(greet))
 
-        count = 0;
+        count = 0
 
         while (True):
             count += 1
 
             response = neuralNetwork(json.loads(await websocket.recv()))
             print(response)
-            print(str(count) + "message exchanged")
-            await websocket.send(json.dumps(response));
+            print(str(count) + 'message exchanged')
+            await websocket.send(json.dumps(response))
 
 
-
-asyncio.run(handle("ws://localhost:8080"))
+asyncio.run(handle('ws://localhost:8080'))
